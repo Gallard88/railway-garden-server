c84787ebe008dfcd3f65f28d1f1912c501cfe322
Tom Burns <Tom.Burns@MadPaws.com.au>
2026-05-26 21:01:26 +1000

    Tweaked Cron timings

--- cmd/api/cron.go
+++ cmd/api/cron.go
@@ -10,20 +10,20 @@ import (
 func (app *application) setupCronJobs() {
 	// Example 1: Run every minute
 
-	app.cron.AddFunc("*/15 * * * *", func() {
+	app.cron.AddFunc("7/15 * * * *", func() {
 		//log.Println("Running task: fetchWeatherForAllLocations() ")
 		app.fetchWeatherForAllLocations(app.db)
 	})
 	app.fetchWeatherForAllLocations(app.db)
 
-	_, err := app.cron.AddFunc("CRON_TZ=Australia/Sydney 0 5 * * *", func() {
+	_, err := app.cron.AddFunc("CRON_TZ=Australia/Sydney 17 5 * * *", func() {
 		log.Println("Running task: fetchRainfallForAllLocations() ")
 		app.fetchRainfallForAllLocations(app.db)
 	})
 	if err != nil {
 		log.Printf("Error scheduling rainfall fetch job: %v", err)
 	}
-	app.fetchRainfallForAllLocations(app.db)
+	go app.fetchRainfallForAllLocations(app.db)
 
 }
 
