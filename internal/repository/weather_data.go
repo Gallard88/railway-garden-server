cd870749730db7f10cd5e44a6458faf77bf9ce4b
Tom Burns <Tom.Burns@MadPaws.com.au>
2026-06-21 09:12:49 +1000

    Reordered data

--- internal/repository/weather_data.go
+++ internal/repository/weather_data.go
@@ -31,7 +31,7 @@ func (r *weatherRecordRepository) List(ctx context.Context, id uint) ([]models.W
 	var WeatherRecords []models.WeatherRecord
 	err := r.db.WithContext(ctx).
 		Where("location_id = ?", id).
-		Order("time DESC").
+		Order("time ASC").
 		Limit(maxRecords).
 		Find(&WeatherRecords).Error
 	return WeatherRecords, err
@@ -55,7 +55,7 @@ func (r *weatherRainfallRepository) List(ctx context.Context, id uint) ([]models
 	var Rainfalls []models.Rainfall
 	err := r.db.WithContext(ctx).
 		Where("location_id = ?", id).
-		Order("time DESC").
+		Order("time ASC").
 		Limit(numberOfDays).
 		Find(&Rainfalls).Error
 	return Rainfalls, err
@@ -66,7 +66,7 @@ func (r *weatherRainfallRepository) RainfallForLocationAndDate(ctx context.Conte
 	err := r.db.WithContext(ctx).
 		Where("location_id = ?", locationId).
 		Where("time = ?", date).
-		Order("time DESC").
+		Order("time ASC").
 		First(&Rainfalls).Error
 	return Rainfalls, err
 }
